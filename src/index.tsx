d497175a71d1d71f0e84708ae2f2494701b49a6e
EminatarenX <eminataren2002@gmail.com>
2024-07-22 15:31:32 -0600

    anadiendo fixing de camera

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// // import '../init'
-// import * as process from "process";
-// global.process = process;
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
